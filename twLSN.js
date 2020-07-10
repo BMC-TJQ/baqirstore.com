5d93d3f843d5d8a780ecc868e9cd4d141bec70f4
Blackfish <58276684+BMC-TJQ@users.noreply.github.com>
2020-07-10 19:41:00 +0700

    Add files via upload

---
+++ twLSN.js
@@ -0,0 +1,10 @@
+/* Daftar Pengguna Template */
+var twLSN = [
+'https://baqirstore.blogspot.com/',
+'www.baqirstore.blogspot.com/',
+'baqirstore.blogspot.com/',
+'https://baqirstore.com/',
+'www.baqirstore.com/',
+];
+/* URL Pemilik Lisensi */
+var redirectURL = 'http://google.com';
