af7474f784c1105ebb997f455309d87c1fd5ac38
Blackfish <58276684+BMC-TJQ@users.noreply.github.com>
2020-07-10 19:57:35 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,10 +1,8 @@
 /* Daftar Pengguna Template */
 var twLSN = [
-'https://www.baqirstore.blogspot.com/',
-'www.baqirstore.blogspot.com/',
-'baqirstore.blogspot.com/',
+'baqirstore.blogspot.com',
+'www.baqirstore.com',
 'https://www.baqirstore.com',
-'www.baqirstore.com/',
 ];
 /* URL Pemilik Lisensi */
 var redirectURL = 'http://google.com';
